8c0f67bbf7adaa82367ec40440620810d1505b38
Ali7154 <26davisa@mbbc.qld.edu.au>
2024-02-09 15:07:13 +1000

    Added welcome screen

---
+++ Objects/Title.py
@@ -0,0 +1,8 @@
+from GameFrame import RoomObject
+
+class Title(RoomObject):
+    """
+    The object for displaying the title
+    """
+    def __init__(self, room, x, y):
+        RoomObject.__init__(self, room, x, y)
--- Rooms/Welcomescreen.py
+++ Rooms/Welcomescreen.py
@@ -1,8 +1,8 @@
 from GameFrame import Level
 
-class WelcomeScreen(Level)
-"""
-Initial screen for the game
-"""
-def __init__(self, screen, joysticks):
-    Level.__init__(self, screen, joysticks)
+class WelcomeScreen(Level):
+    """
+    Intial screen for the game
+    """
+    def __init__(self, screen, joysticks):
+        Level.__init__(self, screen, joysticks)
